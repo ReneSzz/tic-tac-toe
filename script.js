36b2765fc57a38efc96af6b1ce2761ca7b60d078
Rene <renesuar99@gmail.com>
2022-08-05 20:28:28 -0400

    added functionality for declaring winner

--- script.js
+++ script.js
@@ -4,38 +4,71 @@
 
 
 
-
-
-
 const gameBoardCreator = () => {
 const gameboardArray = ["","","","","","","","",""]
 let marker = "";
+const X_CLASS = 'X'; 
+const circle_Class = "O";
+let circleTurn = false;
+
+const winningCombinations = [
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
 
 const gameBoxs = document.getElementsByClassName("gameBox");
+
+
 let createClickEffect = () =>
 {
    for (let index = 0; index < gameBoxs.length; index++) {
     gameBoxs[index].addEventListener('click', function (e){
      
-    if (marker == "X") {
-        marker = "O"
-        if (gameBoxs[index].innerHTML === "")
-        gameBoxs[index].innerHTML = marker;
-        gameboardArray[index]= (gameBoxs[index].innerHTML);
-        console.log(gameboardArray);
-    }
-    else if (marker == "O" || marker =="")
-    marker = "X"
-    if (gameBoxs[index].innerHTML === "")
-        gameBoxs[index].innerHTML = marker;
-        gameboardArray[index]= (gameBoxs[index].innerHTML);
-        console.log(gameboardArray);
+       
+        const currentClass = circleTurn ? circle_Class : X_CLASS
+        if (gameBoxs[index].innerHTML == ""){
+        gameBoxs[index].classList.add(currentClass);
+        console.log(currentClass);
+        gameBoxs[index].innerHTML = currentClass;
+        if (checkWin(currentClass)){
+            console.log('winner');
+        }
+
+
+        swapTurns();
+        }
+
+
+
+ 
     })
         
     
    }
 
+   function swapTurns(){
+    circleTurn = !circleTurn;
+   }
+
 }
+
+function checkWin(currentClass){
+    return winningCombinations.some(combination =>{
+        return combination.every(index =>  {
+        return gameBoxs[index].classList.contains(currentClass)
+    })
+    })
+    
+}
+
 
 return {createClickEffect};
 }
